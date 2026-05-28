70850fc215b024ce2a6a7ecec03ce8397424c2b5
Warren Gunawan <warrengunawan@Warrens-MacBook-Air-2.local>
2026-05-28 16:22:23 -0700

    Created simple flask server to run website and camera

---
+++ backend/server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_socketio import SocketIO
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=5000)
