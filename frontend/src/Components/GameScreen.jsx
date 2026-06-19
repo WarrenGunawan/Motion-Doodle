2619f4a6ee3f5b721b3d9806d1b7b0c4431f82e2
Warren Gunawan <warrengunawan@Warrens-MacBook-Air-2.local>
2026-06-19 16:40:58 -0700

    Added round choice and logic

--- frontend/src/Components/GameScreen.jsx
+++ frontend/src/Components/GameScreen.jsx
@@ -8,26 +8,55 @@ import socket from '../socket'
 
 
 
-function GameScreen({ camWidth, camHeight, localStream, isHost, gameStarted, code, drawer, remoteStreams, onCompositeCanvas, shouldClear }) {
+function GameScreen({ camWidth, camHeight, localStream, isHost, gameStarted, code, drawer, remoteStreams, onCompositeCanvas, shouldClear, gameOver, finalScores}) {
+    const [ rounds, setRounds ] = useState(1)
+
     function startGame() {
-        socket.emit('startGame', { roomCode: code })
+        socket.emit('startGame', { roomCode: code, numRounds: rounds })
     }
+
+    if(gameOver) {
+        return (
+            <div className='flex flex-col justify-center items-center bg-blue-500 mx-3' style={{ width: camWidth, height: camHeight }}>
+                <h2>Game Over!</h2>
+                {finalScores.sort((a, b) => b.score - a.score)
+                    .map(p => (
+                        <p key={p.id}>{p.username}: {p.score}</p>
+                    ))
+                }
+            </div>
+        )
+    }
+
 
     if (!gameStarted) {
         if (isHost) {
             return (
-                <div className='flex justify-center items-center bg-blue-500' style={{ width: camWidth, height: camHeight }}>
+                <div className='flex flex-col justify-center items-center bg-blue-500 mx-3' style={{ width: camWidth, height: camHeight }}>
+                    <div>
+                        <p>Value: {rounds}</p>
+                        <input type='range' id='numericSlider' min='1' max='6' value={rounds} onChange={(e) => {setRounds(Number(e.target.value))}}/>
+                    </div>
+
                     <button className='flex p-5 justify-center items-center bg-black text-white rounded' onClick={startGame}>Start Game</button>
                 </div>
             )
         } else {
-            return <div className='flex justify-center items-center bg-blue-500' style={{ width: camWidth, height: camHeight }}/>
+            return <div className='flex justify-center items-center bg-blue-500 mx-3' style={{ width: camWidth, height: camHeight }}/>
         }
     } else {
         if (drawer.id === socket.id) {
-            return <Camera camWidth={camWidth} camHeight={camHeight} canDraw={true} stream={localStream} onCompositeCanvas={onCompositeCanvas} shouldClear={shouldClear}/>
+            return (
+                <div className='mx-3'>
+                    <Camera camWidth={camWidth} camHeight={camHeight} canDraw={true} stream={localStream} onCompositeCanvas={onCompositeCanvas} shouldClear={shouldClear}/>
+                </div>
+            )
         } else {
-            return <RemoteCam stream={remoteStreams[drawer.id]} camWidth={camWidth} camHeight={camHeight}/>
+            return (
+                <div className='mx-3'>
+                    <RemoteCam stream={remoteStreams[drawer.id]} camWidth={camWidth} camHeight={camHeight}/>
+                </div>
+            )
         }
     }
 }
