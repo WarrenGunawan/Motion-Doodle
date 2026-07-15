df72031f5e3e7804689009611e4643194031afdf
WarrenGunawan <g.warren6501@gmail.com>
2026-07-14 23:12:18 -0700

    Changed port

--- backend/server.py
+++ backend/server.py
@@ -5,6 +5,7 @@ import string
 import json
 import random
 import threading
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -349,7 +350,6 @@ if __name__ == '__main__':
     socketio.run(
         app,
         host="0.0.0.0",
-        debug=True,
-        port=5001,
+        port=int(os.environ.get("PORT", 5001)),
         allow_unsafe_werkzeug=True
     )
